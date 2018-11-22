ecc2f340cde1476aded3629e799f69bf1b6f82d8
Jesus Andres Bernal Lopez <jabernall24@gmail.com>
2018-11-21 20:53:13 -0800

    Initial Commit

---
+++ Lakers Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lakers Trivia
+//
+//  Created by Jesus Andres Bernal Lopez on 11/21/18.
+//  Copyright © 2018 Jesus Andres Bernal Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
